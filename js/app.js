ee009f7ce7be459f49c0d889c7e05f4ec0a03aed
Gabriel Song <gabsong4@gmail.com>
2019-11-07 13:57:31 -0800

    Implement jQuery code in app.js file
    The jQuery <script> within index.html is now in app.js, which makes easier
    to read the JavaScript code outside the content.
    
    buildTweet() function refactored to buildTweetTxt() to return text instead
    of a DOM element. Reason is to append (or prepend) once instead of calling
    jQuery methods on each iteration as multiple tweets must be processed.
    
    getTweet() function looks at the local storage arrays streams.home and
    streams.users[handle] and call buildTweet() within to get more tweets.

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// http://gregfranko.com/jquery-best-practices/
+// IIFE - Immediately Invoked Function Expression
+( (yourcode) => {
+
+  // The global jQuery object is passed as a parameter
+  yourcode(window.jQuery, window, document);
+
+}) ( ($, window, document) => {
+
+  // The $ is now locally scoped
+  $( () => { /* The DOM is ready! */ } );
+
+  // The rest of your code goes here!
+  const $body = $('body');
+  const $stream = $('#stream');
+
+  // create tweet as HTML
+  const buildTweetTxt = (user, message, ts) => {
+    return `<div class="tweet">
+      <p class="handle">@${user}</p>
+      <p class="message">${message}</p>
+      <p class="timestamp">${ts}</p>
+      </div>`.trim();
+  };
+
+  // create tweet as DOM object
+  // const buildTweetObj = (user, message, ts) => {
+  //   let $tweet = $('<div></div>').addClass('tweet');
+  //   const $user = $('<p></p>').addClass('handle').text('@' + user);
+  //   const $message = $('<p></p>').text(message);
+  //   const $ts = $('<p></p>').addClass('timestamp').text(ts);
+  //   $user.appendTo($tweet);
+  //   $message.appendTo($tweet);
+  //   $ts.appendTo($tweet);
+  //   return $tweet;
+  // };
+
+  // loads initial set of tweets (total: 11)
+  let index = streams.home.length - 1;
+  let lastTweet = {
+    home: streams.home.length
+  };
+  while (index >= 0) {
+    const tweet = streams.home[index];
+    $stream.append(buildTweetTxt(tweet.user, tweet.message, tweet.created_at));
+    index -= 1;
+  }
+
+  const getTweet = (start, end, handle) => {
+    let tweets = '';
+    let collection = [];
+    if (handle === undefined) {
+      handle = 'home';
+      collection = streams.home;
+    } else {
+      collection = streams.users[handle];
+    }
+
+    (collection).slice(start, end).forEach(tweet => {
+      tweets = buildTweetTxt(tweet.user, tweet.message, tweet.created_at) + tweets;
+    });
+    lastTweet[handle] = end; // side effect
+    return tweets;
+  };
+
+  // try loading tweets on click
+  $('#get-tweets').click( () => {
+    console.log("test event");
+  });
+
+  // show tweets by user
+
+
+  // update home-area to have username and back button
+
+
+  // show user stream when username is clicked
+
+
+  // test new functions
+  console.log(getTweet(0,5));
+  console.log(getTweet(5,10, 'mracus'));
+  console.log(lastTweet);
+
+});
